b1cf2b216b49de800a8ad3352bfec514f9da63cb
MrCra <mrcrabs67forms@outlook.com>
2024-02-09 03:26:13 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {JSX} from 'react';
 import './App.css';
 import { Box, Text, Button } from '@chakra-ui/react'
+import {useState} from "react";
 import Number from "./components/numbers";
 
 
 const App:React.FunctionComponent = () => {
 
-    const nums: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    //const nums : number = 1;
+    const [counts, setCounts] = useState(0);
+    const [result, setResult] = useState('');
+
 
   return (
     <div className="App">
-      <Box display='flex' justifyContent = 'center' alignItems='center' h='100vh'>
-        {/*<Text fontSize = "2xl">Hello!</Text>*/}
-
-        <Box display='flex' flexWrap='wrap' w='15%'>
-            {nums.map((num) => {
-                return (<Number numbers={num}/>)
-            })}
-            {/*<Number numbers={nums}/>*/}
-        </Box>
+      <Box display='flex' flexDirection='column' justifyContent = 'center' alignItems='center' h='100vh'>
+          <Box display='flex' gap='5px' flexDirection='column' justifyContent='center' alignItems='center' w='200px'>
+              <Box display='flex' justifyContent='between' w='100%' bg = 'gray.50' borderRadius='8px'>
+                  <Text display='flex' justifyContent='start' alignItems='center'  w='100%' h='38px' px='4px' >
+                      {counts}
+                  </Text>
+                  <Text display = 'flex' justifyContent='end' w='fit-content' h='38px' textColor='tomato' alignItems='center'>
+                      {result}
+                  </Text>
+              </Box>
+                <Number data={counts} onClick={setCounts}/>
+          </Box>
       </Box>
 
     </div>
--- src/components/numbers.tsx
+++ src/components/numbers.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import {Button} from "@chakra-ui/react";
+import {Box, Button} from "@chakra-ui/react";
+import {data} from "cheerio/lib/api/attributes";
 
 interface Props {
-    numbers: number;
+    data: number | string;
+    onClick?: any;
 }
-const Number = ({numbers}: Props) =>
-{
-    return(
-//         numbers.map((numb) => {
-//             return <Button>{numb}</Button>
+// const Number = ({numbers, data}: Props) =>
+// {
+//     return(
+// //         numbers.map((numb) => {
+// //             return <Button>{numb}</Button>
+// // }
+// //     )
+//         <Button key = {numbers} w ='40px' h ='40px' margin='4px'>{numbers}</Button>
+//     );
 // }
-//     )
-        <Button key = {numbers} w ='40px' h ='40px' margin='4px'>{numbers}</Button>
-    );
+
+const number = ({data, onClick} : Props) => {
+    const numbers: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const nums = numbers.map((num) => {
+        return (<Button key = {num} w ='40px' h ='40px' margin='4px'
+        onClick={(e) => {
+            console.log('asdf = ', num);
+            debugger;
+            if(data == 0) onClick(num)
+            else {
+                onClick(`${data}${num}`);
+                console.log('!=0')
+            }
+            //data = 0 ? onClick(num) : onClick(`${data}${num}`);
+            // onClick(data + num);
+        }
+        }>{num}</Button>)
+    });
+    return <Box>{nums}</Box>
 }
 
-export default Number;
+export default number;
