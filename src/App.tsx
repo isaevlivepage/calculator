72a2b0794fc3bfe97e2e7b58c47e8e8290deaa6d
MrCra <mrcrabs67forms@outlook.com>
2024-02-12 12:24:22 +0300

    add sum

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box, Text } from '@chakra-ui/react'
 import {useState} from "react";
 import Number from "./components/numbers";
 import CountButton from "./components/countbutton";
+import {data} from "cheerio/lib/api/attributes";
 
 
 const App:React.FunctionComponent = () => {
@@ -10,8 +11,13 @@ const App:React.FunctionComponent = () => {
     const [number, setNumber] = useState('0');
     const [result, setResult] = useState('');
 
+    const applyExpression = (countedNumber: string) => {
+        setNumber(countedNumber);
+        setResult(eval(number));
+    }
 
-  return (
+
+    return (
     <div className="App">
       <Box display='flex' flexDirection='column' justifyContent = 'center' alignItems='center' h='100vh'>
           <Box display='flex' gap='5px' flexDirection='column' justifyContent='center' alignItems='baseline' w='200px'>
@@ -20,16 +26,16 @@ const App:React.FunctionComponent = () => {
                       {number}
                   </Text>
                   <Text display = 'flex' justifyContent='end' w='fit-content' h='38px' textColor='tomato' alignItems='center'>
-                      123 {result}
+                      {result}
                   </Text>
               </Box>
 
               <Number number={number} setNumber={setNumber}/>
               <Box display='flex' flexDirection='row'>
-                  <CountButton expression={'+'}/>
-                  <CountButton expression={'-'}/>
-                  <CountButton expression={'*'}/>
-                  <CountButton expression={'/'}/>
+                  <CountButton data = {number} expression={'+'} applyExpression = {applyExpression}/>
+                  {/*<CountButton expression={'-'}/>*/}
+                  {/*<CountButton expression={'*'}/>*/}
+                  {/*<CountButton expression={'/'}/>*/}
               </Box>
           </Box>
       </Box>
--- src/components/countbutton.tsx
+++ src/components/countbutton.tsx
@@ -1,13 +1,18 @@
 import {Button} from "@chakra-ui/react";
 import React from 'react';
 
+
 type Props = {
     expression: string;
+    data: string;
+    applyExpression: any;
 }
 
-const CountButton = ({expression} : Props) => {
+const CountButton = ({expression, data, applyExpression} : Props) => {
     return(
-        <Button key = {expression} w ='40px' h ='40px' margin='4px'>{expression}</Button>
+        <Button key = {expression} w ='40px' h ='40px' margin='4px' onClick={() => applyExpression(data + expression)}>
+            {expression}
+        </Button>
     )
 }
 
